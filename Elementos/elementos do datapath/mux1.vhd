a37ae514fbb9da401e5076a973e2d3a8b255e0a8
MayaraLima <40068600+MayaraLima@users.noreply.github.com>
2019-12-06 19:00:58 -0300

    Mudança do Mux

--- Elementos/elementos do datapath/mux1.vhd
+++ Elementos/elementos do datapath/mux1.vhd
@@ -2,21 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux1 is port(
+entity mux is port(
 	sel: in std_logic;
-	IN0 : in std_logic_vector(31 downto 0);
+	IN0 : in std_logic_vector(63 downto 0);
 	IN1 : in std_logic_vector(63 downto 0);
 	S : buffer std_logic_vector(63 downto 0)
 );
-end mux1;
+end mux;
 
-architecture hardware of mux1 is
-signal extensao : std_logic_vector(31 downto 0);
+architecture hardware of mux is
 begin
-	WITH IN0(31) SELECT
-		extensao <=	"11111111111111111111111111111111" WHEN '1',
-						"00000000000000000000000000000000" WHEN OTHERS;
 	WITH sel SELECT
 		S <=	IN1 WHEN '1',
-				extensao & IN0 WHEN OTHERS;
+				IN0 WHEN OTHERS;
 end hardware;
